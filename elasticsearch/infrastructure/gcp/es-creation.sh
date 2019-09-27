47fd8d77e7c459cb3162bbadf7e6a093773368b3
Matthew Morris <matthew.morris@cessda.eu>
2019-09-27 10:21:49 +0200

    Use Spring weak binding to configure the GUI component of CVS. This is to remove environment specific configuration from source repositories. Add ES exporter to the ES deployment.

--- elasticsearch/infrastructure/gcp/es-creation.sh
+++ elasticsearch/infrastructure/gcp/es-creation.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -eu
 
 #############################
 #          CESSDA           #
@@ -20,8 +21,10 @@ fi;
 
 ### Kubernetes configuration generation ###
 sed "s#DEPLOYMENTNAME#$product_name-$module_name#g; s#NAMESPACE#$product_name#g" ../k8s/template-statefulset.yaml > ../k8s/$product_name-$module_name-statefulset.yaml
+sed "s#DEPLOYMENTNAME#$product_name-$module_name#g; s#NAMESPACE#$product_name#g" ../k8s/template-deployment.yaml > ../k8s/$product_name-$module_name-deployment.yaml
 sed "s/SERVICENAME/$product_name-$module_name/g; s/NAMESPACE/$product_name/g" ../k8s/template-service.yaml > ../k8s/$product_name-$module_name-service.yaml
 sed "s/SERVICENAME/$product_name-$module_name/g; s/NAMESPACE/$product_name/g" ../k8s/template-discovery.yaml > ../k8s/$product_name-$module_name-discovery.yaml
+sed "s/SERVICENAME/$product_name-$module_name/g; s/NAMESPACE/$product_name/g" ../k8s/template-exporter-service.yaml > ../k8s/$product_name-$module_name-exporter-service.yaml
 sed "s/DEPLOYMENTNAME/$product_name-$module_name/g; s/NAMESPACE/$product_name/g" ../k8s/template-configmap.yaml > ../k8s/$product_name-$module_name-configmap.yaml
 sed "s/DEPLOYMENTNAME/$product_name-$module_name/g; s/NAMESPACE/$product_name/g" ../k8s/template-secret.yaml > ../k8s/$product_name-$module_name-secret.yaml
 
@@ -33,7 +36,9 @@ kubectl apply -f ../k8s/$product_name-$module_name-secret.yaml
 
 # Deployment
 kubectl apply -f ../k8s/$product_name-$module_name-statefulset.yaml
+kubectl apply -f ../k8s/$product_name-$module_name-deployment.yaml
 
 # Service
 kubectl apply -f ../k8s/$product_name-$module_name-service.yaml
+kubectl apply -f ../k8s/$product_name-$module_name-exporter-service.yaml
 kubectl apply -f ../k8s/$product_name-$module_name-discovery.yaml
